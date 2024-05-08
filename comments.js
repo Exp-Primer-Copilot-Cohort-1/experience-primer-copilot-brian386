10904853d2adce61a75e6f7ea0b487bc00580e1a
Brian Zhang <55104633+brian386@users.noreply.github.com>
2024-05-08 22:57:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the web server!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
